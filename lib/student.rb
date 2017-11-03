7021efa093beced91a5b990dcbaf02efb81a8009
Ben Blackman <bblkmn5@gmail.com>
2017-11-03 21:11:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_has.each do |key, value|
+    attributes_hash.each do |key, value|
       self.send("#{key}=", value)
     end
   end
